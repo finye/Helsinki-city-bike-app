c3f4495a1418ba05517a3c0acc6a30a489c327c3
finye <finnantiku@gmail.com>
2023-03-28 22:09:40 +0300

    adds a monorepo using lerna

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -24,6 +24,6 @@ const getDataFromTable = async (req: Request, res: Response, tableName: string) 
     }
 }
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(3001, () => {
+    console.log('Server is listening on port 3001');
 });
